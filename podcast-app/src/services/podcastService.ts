6228addd08a0c80020036b2a85603c36156f54c9
Yonela Mazele <mazeleyonela8@gmail.com>
2025-04-28 12:35:46 +0200

    fetch url

---
+++ podcast-app/src/services/podcastService.ts
@@ -0,0 +1,11 @@
+const BASE_URL = "https://podcast-api.netlify.app";
+
+export async function fetchPreviews() {
+  const res = await fetch(`${BASE_URL}`);
+  return res.json();
+}
+
+export async function fetchShowById(id: number) {
+  const res = await fetch(`${BASE_URL}/id/${id}`);
+  return res.json();
+}
